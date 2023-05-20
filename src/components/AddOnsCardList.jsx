3af6323847080861163644a7e2f86acd62a9127e
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-05-20 00:16:42 +0000

    summary and thanks mobile view added

--- src/components/AddOnsCardList.jsx
+++ src/components/AddOnsCardList.jsx
@@ -91,8 +91,8 @@ const AddOnsCardList = () => {
         <section ref={checkRef} className="flex flex-col gap-4 ">
           {renderCard}
         </section>
-        <section className="absolute w-[95%] lg:w-[100%] top-[25rem] lg:top-[20rem]">
-          <div className="absolute w-[80%] left-[65%] lg:left-[56%]  xl:left-[67%]">
+        <section className="absolute w-[95%] lg:w-[100%] top-[53vh] lg:top-[20rem]">
+          <div className="absolute w-[50%] left-[65%] lg:left-[56%]  xl:left-[67%]">
             <PrimaryButton />
           </div>
           <div onClick={goBackHandler}>
