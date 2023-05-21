6c4feed032c75a79a151e7e28d0102db8d4dc340
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-05-21 22:01:33 +0000

    thanks page modified

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ThankYouImage from "../assets/images/icon-thank-you.svg"
 const Thanks = () => {
   return (
-    <main className='flex flex-col justify-center items-center text-center w-[89%] lg:w-[75%] mx-auto h-[25rem] lg:h-0 lg:mt-[17rem] font-ubuntu lg:ml-[8%] font-semibold'>
+    <main className='flex flex-col justify-center items-center text-center w-[90.8%] lg:w-[75%] mx-auto h-[25rem] lg:h-0 lg:mt-[17rem] font-ubuntu lg:ml-[8%] font-semibold'>
         <div className=''><img className='w-[4rem] mb-[2rem]'  src={ThankYouImage} alt="Thank you" /></div>
         <h1 className='mb-2 text-MarineBlue text-3xl font-bold'>Thank You</h1>
         <p className='text-black/30'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
